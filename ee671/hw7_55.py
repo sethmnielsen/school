a0cc7ee7d58bfff13cc6023fa15f390b24fcfd69
Seth Nielsen <sethmnielsen@gmail.com>
2018-11-14 12:39:01 -0700

    Robotics hw6 continue

--- ee671/hw7_55.py
+++ ee671/hw7_55.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-m = 3
-n = 10000
+m = 1  # number of parameters
+n = 10000  # iterations
 
-h = np.array([1,2,3,4,5])
-hh = np.zeros(m)
+h = np.array([1,2,3,4,5])  # impulse response
+hh = np.zeros(m)  # hhat - estimated parameters
 f = np.random.randn(n)
-fn = np.hstack(([0,0,0,0],f))
+fn = np.hstack(([0,0,0,0],f))  # fnew
 delta = .0001
 P = 1/delta * np.eye(m)
 q = np.zeros(m)
 
 for i in range(n):
-    d = fn[i:i+5] @ h
-    q = np.hstack((f[i],q[:-1]))
+    d = fn[i:i+5] @ h  # fn update
+    q = np.hstack((f[i],q[:-1]))  # q update
 
     k = P @ q / (1 + q.T @ P @ q)
     e = d - q @ hh
