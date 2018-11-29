ab299a862a822792ed19d137210347c87648beb8
Seth Nielsen <sethmnielsen@gmail.com>
2018-11-29 16:50:46 -0700

    671 Exam2 finished

--- MATLAB/Robotics/HW6/hw6_3b.m
+++ MATLAB/Robotics/HW6/hw6_3b.m
@@ -4,32 +4,27 @@ robot = p560;
 load puma560_torque_profile.mat
 
 dt = 0.01;
+t = time(1:end-1);
 qdd_o = zeros(length(torque)-1,6);
-for i=2:length(torque)-1
-    qdd_o(i,:) = qdd_o(i-1,:) + (torque(i-1,:) + torque(i,:))/2 * dt;
-end
-
 qd_o = zeros(length(torque)-1,6);
-for i=2:length(torque)-1
-    qd_o(i,:) = qd_o(i-1,:) + (qdd(i-1,:) + qdd(i,:))/2 * dt;
-end
-
 q_o = zeros(length(torque)-1,6);
 for i=2:length(torque)-1
-    q_o(i,:) = q_o(i-1,:) + (qd(i-1,:) + qd(i,:))/2 * dt;
+    qdd_o(i,:) = p560.accel(q_o(i-1,:), qd_o(i-1,:), torque(i-1,:));
+    qd_o(i,:) = qd_o(i-1,:) + qdd_o(i,:)*dt;
+    q_o(i,:) = q_o(i-1,:) + qd_o(i,:)*dt;
 end
 
-subplot(2,2,1)
+figure(1)
 plot(t,q_o)
 xlabel('t')
 ylabel('q')
 title('q from integration')
-subplot(2,2,2)
+figure(2)
 plot(t,qd_o)
 xlabel('t')
 ylabel('qd')
 title('qdot from integration')
-subplot(2,2,3)
+figure(3)
 plot(t,qdd_o)
 xlabel('t')
 ylabel('qdd')
