d3e893d48683697d4f449245d8cdd24b3bd3242b
Seth Nielsen <sethmnielsen@gmail.com>
2019-01-23 13:19:42 -0700

    Removed old folder

--- old/me537/Robotics_matlab/HW6/myfun.m
+++
@@ -1,4 +0,0 @@
-function f=myfun(q, qd)
-    dt = 0.01;
-    f = q
-end
--- old/me537/Robotics_matlab/hw1.m
+++
@@ -1,9 +0,0 @@
-d1_ = 1; a2_ = 1; d3_ = 1; d4_ = 2; a5_ = d3_; d6_ = 2;
-
-% robot6 = SerialLink([0 d1 0 -pi/2; 0 0 a2 0; pi/2 d3 0 pi/2; pi/2 d4 0 pi/2; 0 0 -a5 -pi/2; 0 d6 0 0], 'name', 'robot6');
-robot6 = SerialLink([0 d1_ 0 pi/2; 0 0 a2_ 0; pi/2 d3_ 0 pi/2; pi/2 d4_ 0 pi/2; 0 0 -a5_ -pi/2; 0 d6_ 0 0], 'name', 'robot6');
-robot6.links(3).offset = pi/2;
-robot6.links(4).offset = pi/2;
-
-% robot6.fkine([th1 th2 th3 th4 th5 th6])
-robot6.fkine([0 0 0 0 0 0])
