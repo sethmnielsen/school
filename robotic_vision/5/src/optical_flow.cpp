ca7f16981b0a29ee00036d73fa7c3e9628a02e68
Seth Nielsen <sethmnielsen@gmail.com>
2019-03-19 12:58:37 -0600

    Auto-pushed changes

---
+++ robotic_vision/5/src/optical_flow.cpp
@@ -0,0 +1,16 @@
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+    VideoCapture cap;
+    TermCriteria termcrit(TermCriteria::COUNT|TermCriteria::EPS,20,0.03);
+
+    
+    
+    return 0;
+}
