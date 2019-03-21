ab1ac436cd346890e35a6f517f1811e18ff77703
Seth Nielsen <sethmnielsen@gmail.com>
2019-03-21 10:31:26 -0600

    Fixing large file commit

--- robotic_vision/5/src/optical_flow.cpp
+++ robotic_vision/5/src/optical_flow.cpp
@@ -1,44 +1,86 @@
 #include <opencv2/opencv.hpp>
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 using namespace cv;
 
-Point2f point;
-bool addRemovePt = false;
 
-int main(int argc, char** argv, Mat hi)
+int main(int argc, char **argv)
 {
-    VideoCapture cap("MotionFieldVideo.mp4");
-    TermCriteria termcrit(TermCriteria::COUNT|TermCriteria::EPS,20,0.03);
-    Size subPixWinSize(10,10), winSize(31,31);
-    
-    int MAX_FEATURES = 500;
-    bool init = false;
 
-    namedWindow( "LK Optical Flow", 1 );
-    vector<Point2f> points[2];    
+  int skip_frames = 0;
+  if ( argc > 1)
+    skip_frames = stoi(argv[1]);
+  else
+    skip_frames = 10;
     
-    Mat gray, prev_gray, img, frame;   
+  VideoCapture cap("/home/seth/school/robotic_vision/5/MotionFieldVideo.mp4");
+  Size subPixWinSize(10, 10), winSize(21, 21);
 
-    for(;;)
+  TermCriteria crit{TermCriteria::COUNT + TermCriteria::EPS, 40, 0.001};
+  int MAX_CORNERS = 500;
+  double quality(0.01), min_dist(10.0), min_eig(0.02);
+
+  vector<Point2f> corners;
+  vector<Point2f> prev_corners;
+
+  namedWindow("prev_gray", WINDOW_NORMAL | WINDOW_KEEPRATIO | WINDOW_GUI_EXPANDED);
+  namedWindow("gray", WINDOW_NORMAL | WINDOW_KEEPRATIO | WINDOW_GUI_EXPANDED);
+  
+  Mat gray, prev_gray, frame;
+  int frame_num = 0;
+  cout << "-- Beginning launch sequence..." << endl;
+  while (true)
+  {
+    if ( frame.empty() )
     {
-        cap >> frame;
-        if ( frame.empty() )
-            break;
-        
-        frame.copyTo(img);
-        cvtColor(img, gray, COLOR_BGR2GRAY);
+      cap >> frame;
+      cvtColor(frame, prev_gray, COLOR_BGR2GRAY);
+    }
+      
+    for (int i=0; i < skip_frames; i++)
+      cap >> frame;
+    if (frame.empty())
+    {
+      cout << "-- Main engine cutoff"; 
+      break;
+    }
 
-        if ( init )
-        {
-            goodFeaturesToTrack( gray, points[1], MAX_FEATURES, 0.01, 10, Mat(), 3, 0, 0.04 );
-            cornerSubPix(gray, points[1], subPixWinSize, Size(-1,-1), termcrit);
-            
-        }
-        
+    if ( !gray.empty() )
+      gray.copyTo(prev_gray);
+    cvtColor(frame, gray, COLOR_BGR2GRAY);
+    goodFeaturesToTrack(prev_gray, prev_corners, MAX_CORNERS, quality, min_dist);
+    // cornerSubPix(prev_gray, prev_corners, subPixWinSize, Size(-1, -1), crit);
+
+    vector<uchar> status;
+    vector<float> err;
+    cv::imshow("prev_gray", prev_gray);
+    cv::imshow("gray", gray);
+    waitKey(1);
+    calcOpticalFlowPyrLK(prev_gray, gray, prev_corners, corners, status, err, winSize,
+                         0, crit, 0, min_eig);
+
+    int counter(0);
+    for (int i=0; i < prev_corners.size(); i++)
+    {
+      // if ( !status[i] )
+      //   continue;
+      if ( status[i] && err[1] < 20 )
+      {
+        circle(frame, prev_corners[i], 3, Scalar(0,255,0), -1);
+        line(frame, prev_corners[i], corners[i], Scalar(0,0,255), 1);
+        counter++;
+      }
     }
     
-    return 0;
+    cv::imshow("Optical Flow", frame);
+    char c = (char)waitKey(1);
+    if ( c == 'q' )
+      break;
+  }
+  cap.release();
+
+  return 0;
 }
