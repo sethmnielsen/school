bfaf16151dc20f887c75b1050acf3de557d52554
Seth Nielsen <sethmnielsen@gmail.com>
2019-03-22 13:01:12 -0600

    Auto-pushed changes

--- robotic_vision/5/src/feature_matching.cpp
+++ robotic_vision/5/src/feature_matching.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 using namespace cv;
 
+Point2f point_corrected(Point2f pt, int w, Mat img);
 
 int main(int argc, char **argv)
 {
@@ -17,18 +18,17 @@ int main(int argc, char **argv)
     skip_frames = 10;
     
   VideoCapture cap("/home/seth/school/robotic_vision/5/MotionFieldVideo.mp4");
-  Size subPixWinSize(10, 10), winSize(21, 21);
+  int w(5);
+  int ws = w*10;
+  Size winSize(21, 21), templateSize(w,w), searchSize(ws,ws);
 
   TermCriteria crit{TermCriteria::COUNT + TermCriteria::EPS, 40, 0.001};
   int MAX_CORNERS(500), PYRAMID_LEVEL(2);
-  double quality(0.01), min_dist(10.0), min_eig(0.02);
+  double QUALITY(0.01), MIN_DIST(10.0), MIN_EIG(0.02);
 
   vector<Point2f> corners;
   vector<Point2f> prev_corners;
 
-  // namedWindow("prev_gray", WINDOW_NORMAL | WINDOW_KEEPRATIO | WINDOW_GUI_EXPANDED);
-  // namedWindow("gray", WINDOW_NORMAL | WINDOW_KEEPRATIO | WINDOW_GUI_EXPANDED);
-  
   Mat gray, prev_gray, frame;
   int frame_num = 0;
   cout << "-- Beginning launch sequence..." << endl;
@@ -51,22 +51,22 @@ int main(int argc, char **argv)
     if ( !gray.empty() )
       gray.copyTo(prev_gray);
     cvtColor(frame, gray, COLOR_BGR2GRAY);
-    goodFeaturesToTrack(prev_gray, prev_corners, MAX_CORNERS, quality, min_dist);
-    // cornerSubPix(prev_gray, prev_corners, subPixWinSize, Size(-1, -1), crit);
+    goodFeaturesToTrack(prev_gray, prev_corners, MAX_CORNERS, QUALITY, MIN_DIST);
 
     vector<uchar> status;
     vector<float> err;
-    // cv::imshow("prev_gray", prev_gray);
-    // cv::imshow("gray", gray);
-    // waitKey(1);
-    calcOpticalFlowPyrLK(prev_gray, gray, prev_corners, corners, status, err, winSize,
-                         PYRAMID_LEVEL, crit, 0, min_eig);
 
     int counter(0);
     for (int i=0; i < prev_corners.size(); i++)
     {
-      if ( !status[i] )
-        continue;
+      Point2f pt = point_corrected(prev_corners[i], w, frame);
+      Rect templ_roi(pt, templateSize);
+      Mat templ = prev_gray(templ_roi);
+      Point2f search_pt = point_corrected(prev_corners[i], ws, frame);
+      Rect search_roi(search_pt, searchSize);
+      Mat search_box = gray(search_roi);
+
+      
       circle(frame, prev_corners[i], 3, Scalar(0,255,0), -1);
       line(frame, prev_corners[i], corners[i], Scalar(0,0,255), 1);
       counter++;
@@ -81,4 +81,24 @@ int main(int argc, char **argv)
   cap.release();
 
   return 0;
+}
+
+Point2f point_corrected(Point2f pt, int w, Mat img)
+{
+  double x, y;
+
+  if(pt.x > img.cols - w/2.0)
+    x = img.cols - w;
+  else if(pt.x < w/2.0)
+    x = 0;
+  else
+    x = pt.x- w/2.0;
+  if(pt.y > img.rows - w/2.0)
+    y = img.rows - w;
+  else if(pt.y < w/2.0)
+    y = 0;
+  else
+     y = pt.y - w/2.0;
+
+  return cv::Point2f(x, y);
 }
