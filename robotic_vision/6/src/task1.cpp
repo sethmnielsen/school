c9c88de6ac43dd511a71f69c6ed7e158313b52fa
Seth Nielsen <sethmnielsen@gmail.com>
2019-03-29 15:43:33 -0600

    Auto-pushed changes

--- robotic_vision/6/src/task1.cpp
+++ robotic_vision/6/src/task1.cpp
@@ -7,35 +7,19 @@ using namespace std;
 using namespace cv;
 
 Point2f point_corrected(Point2f pt, int w, Mat img);
-vector<Mat> motion_field(int m);
-void record_video(vector<Mat> vid1, vector<Mat> vid2, string filename); 
+void motion_field();
+void display_img(Mat img);
 
 int main()
 {
-  std::vector<cv::Mat> vid1;
-  vid1 = motion_field(5);
+  motion_field();
   return 0;
 }
 
-void record_video(vector<Mat> vid1, vector<Mat> vid2, string filename)
+void motion_field()
 {
-  int cc = cv::VideoWriter::fourcc('M', 'P', 'E', 'G');
-  Size size(1920, 1080);
-  VideoWriter vid_out(filename, cc, 20, size, true);
-
-  for (int i=0; i < vid1.size(); i++)
-    vid_out << vid1[i];
-  for (int i=0; i < vid2.size(); i++)
-    vid_out << vid2[i];
-  vid_out.release();
-}
-
-vector<Mat> motion_field(int m)
-{
-  VideoCapture cap("/home/seth/school/robotic_vision/6/imgs/ParallelCube1%0d.jpg");
-  int frame_count = int(cap.get(cv::CAP_PROP_FRAME_COUNT)) / m;
-  cout << "Frame count: " << frame_count << endl;
-  vector<Mat> vid(frame_count);
+  VideoCapture cap("/home/seth/school/robotic_vision/6/imgs/TurnCube1%0d.jpg");
+  int m = cv::CAP_PROP_FRAME_COUNT;
   
   int w(5);
   int ws = w*11;
@@ -48,94 +32,87 @@ vector<Mat> motion_field(int m)
   vector<Point2f> new_corners, prev_corners, orig_corners, temp;
   vector<uchar> mask;
 
-  int count = 0;
+  Mat F, H1, H2; // fundamental matrix, homography matrices
 
   cout << "-- Auto sequence start..." << endl;
-  for (int k=0; k < frame_count; k++)
+  Mat frame, gray, prev_gray;
+  cap >> frame;
+  cvtColor(frame, gray, COLOR_BGR2GRAY);
+    
+  gray.copyTo(prev_gray);
+  goodFeaturesToTrack(prev_gray, prev_corners, MAX_CORNERS, QUALITY, MIN_DIST);
+  orig_corners = prev_corners;
+
+  for (int i=0; i < orig_corners.size(); i++)
   {
-    Mat frame, gray, prev_gray;
+    circle(frame, orig_corners[i], 3, Scalar(0,255,0), -1);
+  }
+
+  display_img(frame);
+
+  for (int j=1; j < m; j++)
+  {
     cap >> frame;
-    cvtColor(frame, prev_gray, COLOR_BGR2GRAY);
-      
-    if ( !gray.empty() )
-      gray.copyTo(prev_gray);
+    if (frame.empty())
+      break;
+
     cvtColor(frame, gray, COLOR_BGR2GRAY);
-    goodFeaturesToTrack(prev_gray, prev_corners, MAX_CORNERS, QUALITY, MIN_DIST);
-    orig_corners = prev_corners;
-
-    for (int j=1; j < m; j++)
+    
+    new_corners.clear();
+    for (int i=0; i < prev_corners.size(); i++)
     {
-      cap >> frame;
-      if (frame.empty())
-        break;
+      Point2f pt = point_corrected(prev_corners[i], w, frame);
+      Rect templ_roi(pt, templateSize);
+      Mat templ = prev_gray(templ_roi);
+      Point2f search_pt = point_corrected(prev_corners[i], ws, frame);
+      Rect search_roi(search_pt, searchSize);
+      Mat search_box = gray(search_roi);
 
-      cvtColor(frame, gray, COLOR_BGR2GRAY);
-      
-      new_corners.clear();
-      for (int i=0; i < prev_corners.size(); i++)
-      {
-        Point2f pt = point_corrected(prev_corners[i], w, frame);
-        Rect templ_roi(pt, templateSize);
-        Mat templ = prev_gray(templ_roi);
-        Point2f search_pt = point_corrected(prev_corners[i], ws, frame);
-        Rect search_roi(search_pt, searchSize);
-        Mat search_box = gray(search_roi);
+      int res_cols = search_box.cols - templ.cols + 1;
+      int res_rows = search_box.rows - templ.rows + 1;
+      Mat result;
+      result.create(res_rows, res_cols, CV_32FC1);
+      matchTemplate(search_box, templ, result, match_method);
+      normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, Mat());
 
-        int res_cols = search_box.cols - templ.cols + 1;
-        int res_rows = search_box.rows - templ.rows + 1;
-        Mat result;
-        result.create(res_rows, res_cols, CV_32FC1);
-        matchTemplate(search_box, templ, result, match_method);
-        normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, Mat());
+      double minVal; double maxVal;
+      cv::Point matchLoc, minLoc, maxLoc;
+      cv::minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat());
+      matchLoc.x = prev_corners[i].x - res_cols/2.0 + minLoc.x;
+      matchLoc.y = prev_corners[i].y - res_rows/2.0 + minLoc.y;
 
-        double minVal; double maxVal;
-        cv::Point matchLoc, minLoc, maxLoc;
-        cv::minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat());
-        matchLoc.x = prev_corners[i].x - res_cols/2.0 + minLoc.x;
-        matchLoc.y = prev_corners[i].y - res_rows/2.0 + minLoc.y;
-
-        new_corners.push_back(matchLoc);
-      }
-
-      Mat F = findFundamentalMat(prev_corners, new_corners, cv::FM_RANSAC, 
-                                3, 0.99, mask);
-
-      prev_corners.clear();
-      temp.clear();
-      for (int i=0; i < mask.size(); i++)
-      {
-        if (mask[i])
-        {
-          prev_corners.push_back(new_corners[i]);
-          temp.push_back(orig_corners[i]);
-        }
-      }
-      orig_corners = temp;
-      gray.copyTo(prev_gray);
-      count++;
+      new_corners.push_back(matchLoc);
     }
 
-    if (frame.empty())
-        break;
-    
+    F = findFundamentalMat(prev_corners, new_corners, cv::FM_RANSAC, 
+                              3, 0.99, mask);
+
+    prev_corners.clear();
+    temp.clear();
+    for (int i=0; i < mask.size(); i++)
+    {
+      if (mask[i])
+      {
+        prev_corners.push_back(new_corners[i]);
+        temp.push_back(orig_corners[i]);
+      }
+    }
+    orig_corners = temp;
+    gray.copyTo(prev_gray);
+
     for (int i=0; i < prev_corners.size(); i++)
     {
       circle(frame, orig_corners[i], 3, Scalar(0,255,0), -1);
       line(frame, orig_corners[i], prev_corners[i], Scalar(0,0,255), 1);
     }
-    vid[k] = frame;
-    count++;
+
+    display_img(frame);
+
+    stereoRectifyUncalibrated(orig_corners, prev_corners, F, 
+                              Size(frame.cols, frame.rows), H1, H2);
   }
+
   cap.release();
-  for (int i=0; i < vid.size(); i++)
-  {
-    cv::imshow("Multi-Frame Tracking", vid[i]);
-    char c = (char)waitKey(0);
-    if ( c == 'q' )
-      break;
-  }
-  
-  return vid;
 }
 
 Point2f point_corrected(Point2f pt, int w, Mat img)
@@ -156,4 +133,19 @@ Point2f point_corrected(Point2f pt, int w, Mat img)
      y = pt.y - w/2.0;
 
   return Point2f(x, y);
+}
+
+void display_img(Mat img)
+{
+  if (img.empty())
+  {
+    cout << "Image empty!" << endl;
+    return;
+  }
+  
+  cv::imshow("Multi-Frame Tracking", img);
+  char c = (char)waitKey(0);
+  if ( c == 'q' )
+    destroyAllWindows();
+    exit(0);
 }
