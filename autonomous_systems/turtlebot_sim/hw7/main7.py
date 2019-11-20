ba41632c2c8e5ba2bcdcae3e2c2cab1792b72a3b
Seth Nielsen <sethmnielsen@gmail.com>
2019-11-20 11:55:20 -0600

    Auto-pushed changes

--- autonomous_systems/turtlebot_sim/hw7/main7.py
+++ autonomous_systems/turtlebot_sim/hw7/main7.py
@@ -29,26 +29,33 @@ if __name__ == '__main__':
 
     finished = False
     N = pm.N
-    for i in range(1,N):
-        # i is my timestep
-        state = tbot.states[:,i]
-        
-        fslam.prediction_step(tbot.vc[i-1], tbot.omgc[i-1])
-        z, detected_mask = tbot.get_measurements(state)
-        if xp.any(detected_mask):
-            fslam.measurement_correction(z, detected_mask)
 
-        fslam.compute_eigs()
-        fslam.write_history(i)
 
-        # update plot animation
-        try:
-            plotter.update_ekfs_plot(state, fslam.xhat, fslam.Pa, fslam.P_angs, fslam.w, i)
-        except KeyboardInterrupt:
-            break
+    # ------------------------ BEGIN MAIN LOOP ----------------------------#
+    
+    
+    for i in range(1,N):              # i is timestep
+        for j in range(pm.num_lms):   # j is landmark index
+            state = tbot.states[:,i]  # true state
 
-        if i == N-1:
-            finished = True
+
+            fslam.prediction_step(tbot.vc[i-1], tbot.omgc[i-1]) # propagate all particles forward
+            
+            z, detected_mask = tbot.get_measurements(state)
+            if xp.any(detected_mask):
+                fslam.measurement_correction(z, detected_mask)
+
+            fslam.compute_eigs()
+            fslam.write_history(i)
+
+            # update plot animation
+            try:
+                plotter.update_ekfs_plot(state, fslam.xhat, fslam.Pa, fslam.P_angs, fslam.w, i)
+            except KeyboardInterrupt:
+                break
+
+            if i == N-1:
+                finished = True
 
     # if finished:
         # plotter.make_plots_ekfs(pm.t_arr)
