2c0675c44d366258a03085f1fadcbeb9d205cc5e
Seth Nielsen <sethmnielsen@gmail.com>
2019-12-03 18:10:05 -0700

    Auto-pushed changes

--- autonomous_systems/turtlebot_sim/hw8/mdp.py
+++ autonomous_systems/turtlebot_sim/hw8/mdp.py
@@ -11,18 +11,52 @@ class MDP():
         self.pf = pm.p_forw
         self.pr = pm.p_right
         self.pl = pm.p_left
-        
+        self.iter = 0
 
-    def calcValues(self):
-        inds = np.nonzero(self.map)
         self.rew_map = pm.r_else
         self.value_map = np.copy(self.rew_map)
+
+        
+    def calcValues1(self):
+        diff = 1e6
+        epsilon = 300
+
+        while diff > epsilon:
+            temp_diff = []
+            for i in range(1, pm.rows-1):
+                for j in range(1, pm.cols-1):
+                   if self.rew_map[i,j] == pm.r_else:
+                        V_north = (self.pf * (pm.walls[i+1, j  ] + pm.obs[i+1, j  ] + pm.goal[i+1, j] + self.map[i+1,j]) + \
+                                   self.pr * (pm.walls[i  , j+1] + pm.obs[i  , j+1] + pm.goal[i, j+1] + self.map[i, j+1]) + \
+                                   self.pl * (pm.walls[i  , j-1] + pm.obs[i  , j-1] + pm.goal[i, j-1] + self.map[i, j-1])) + self.rew_map[i,j]
+                        V_south = (self.pf * (pm.walls[i-1, j  ] + pm.obs[i-1, j  ] + pm.goal[i-1, j] + self.map[i-1, j]) + \
+                                   self.pr * (pm.walls[i  , j+1] + pm.obs[i  , j+1] + pm.goal[i, j+1] + self.map[i, j+1]) + \
+                                   self.pl * (pm.walls[i  , j-1] + pm.obs[i  , j-1] + pm.goal[i, j-1] + self.map[i, j-1])) + self.rew_map[i,j]
+                        V_east  = (self.pf * (pm.walls[i  , j+1] + pm.obs[i  , j+1] + pm.goal[i, j+1] + self.map[i, j+1]) + \
+                                   self.pr * (pm.walls[i+1, j  ] + pm.obs[i+1, j  ] + pm.goal[i+1, j] + self.map[i+1, j]) + \
+                                   self.pl * (pm.walls[i-1, j  ] + pm.obs[i-1, j  ] + pm.goal[i-1, j] + self.map[i-1, j])) + self.rew_map[i,j]
+                        V_west  = (self.pf * (pm.walls[i  , j-1] + pm.obs[i  , j-1] + pm.goal[i, j-1] + self.map[i, j-1]) + \
+                                   self.pr * (pm.walls[i-1, j  ] + pm.obs[i-1, j  ] + pm.goal[i-1, j] + self.map[i-1, j]) + \
+                                   self.pl * (pm.walls[i+1, j  ] + pm.obs[i+1, j  ] + pm.goal[i+1, j] + self.map[i+1, j])) + self.rew_map[i,j]
+                        V = [V_north, V_south, V_east, V_west]
+                        max = np.max(V)
+                        argmax = np.argmax(V)
+                        self.policy[i, j] = argmax
+                        temp_diff.append(np.abs(self.map[i,j] - max * self.gamma))
+                        self.map[i,j] = max * self.gamma
+            diff = np.sum(temp_diff)
+            self.iter += 1
+
+
+    def calcValues2(self):
+        inds = np.nonzero(self.map)
+
         diff = 1e6
         epsilon = 1
         idx_r0 = 1
-        idx_r = params.r - 1
+        idx_r = pm.rows - 1
         idx_c0 = 1
-        idx_c = params.c - 1
+        idx_c = pm.cols - 1
 
         while diff > epsilon:
             #Value for the traveling north reward policy. Need to add the cost of being in that state
@@ -44,9 +78,9 @@ class MDP():
             self.map[idx_r0:idx_r, idx_c0:idx_c] = max
 
             #reset obstacles
-            idx_walls = np.argwhere(not params.walls == 0)
+            idx_walls = np.argwhere(not pm.walls == 0)
             self.map[idx_walls[0,:], idx_walls[1,:]] = -100
-            idx_obs = np.argwhere(not params.obs == 0)
+            idx_obs = np.argwhere(not pm.obs == 0)
             self.map[idx_obs[0,:], idx_obs[1,:]] = -5000
-            idx_goal = np.argwhere(not params.goal == 0)
-            self.map[idx_goal[0,:], idx_goal[1,:]] = np.sign(params.goal[idx_goal[0,:], idx_goal[1,:]]) * 1e6
+            idx_goal = np.argwhere(not pm.goal == 0)
+            self.map[idx_goal[0,:], idx_goal[1,:]] = np.sign(pm.goal[idx_goal[0,:], idx_goal[1,:]]) * 1e6
