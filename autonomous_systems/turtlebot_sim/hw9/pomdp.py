be43dc6bac7c14bda3b6942fa150a13b3a2e6190
Seth Nielsen <sethmnielsen@gmail.com>
2019-12-05 12:00:31 -0700

    Auto-pushed changes

---
+++ autonomous_systems/turtlebot_sim/hw9/pomdp.py
@@ -0,0 +1,19 @@
+"""
+1) Implement algorithm 15.1 with the objective of representing the value function for the illustrative example of section 15.2
+
+2) Compute the value function for the example for a time horizon of 2 and show that you obtain the linear value function constraints of equation 15.31 (along with a few others that have not been pruned away).
+
+3) Develop a simple pruning strategy to prune away some of the obviously superfluous constraints (replicates of initial payoff constraints, constraints dominated by (lying below) the payoff constraint for u3).
+
+4) Modify the state transition probability for u3 as well as the measurement probabilities for states x1 and x2. Compute value functions for different probabilities. Do your results make sense? Change the payoffs associated with the control actions and compute the value function. Do the results make sense?
+
+5) Using the probability and payoff parameters of your choice, use the associated value function to choose control actions. Assume that your true initial state is x1 and that your belief is 0.6. What outcomes do you obtain for 10 trials? Do you outcomes align with your expectations? Did your value function produce good results?
+"""
+
+import numpy as np
+import hw9.params as pm
+from utils import wrap
+
+class MDP():
+    def __init__(self):
+        
